b3b7e6d1061f741b331db90bf54fd5aeee26d4a1
Helena Lajevardi <helena.lajevardi@haw-hamburg.de>
2017-10-09 22:22:03 +0200

    Klasse Element und Key geändert

--- src/AB1/Element.java
+++ src/AB1/Element.java
@@ -1,21 +1,37 @@
 package AB1;
+
 /**
- * Element-Klasse.
+ * Diese Klasse repräsentiert ein Element.
  * 
  * @author Helena Lajevardi
  */
 public class Element<T> {
-	
+
 	/**
 	 * Schlüssel des Elements.
 	 */
 	private Key key;
-	
+
 	/**
 	 * Zu speichernder Inhalt.
 	 */
 	private T data;
-	
-	
+
+	public Element(T data) {
+		this(data, new Key());
+	}
+
+	public Element(T data, Key key) {
+		this.data = data;
+		this.key = key;
+	}
+
+	public Key getKey() {
+		return key;
+	}
+
+	public T getData() {
+		return data;
+	}
 
 }
--- src/AB1/Key.java
+++ src/AB1/Key.java
@@ -19,4 +19,6 @@ public class Key {
     return keyValue;
   }
   
+  
+  
 }
