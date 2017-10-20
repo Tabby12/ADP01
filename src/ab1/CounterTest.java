93eb4d1b77d8f0a031f31cb330158ba9eef42fe0
Malte Janssen <malte.janssen@haw-hamburg.de>
2017-10-20 17:38:36 +0200

    CounterTest finished

--- src/ab1/CounterTest.java
+++ src/ab1/CounterTest.java
@@ -1,195 +1,179 @@
 package ab1;
 
+import com.sun.javafx.geom.AreaOp.AddOp;
 
 class CounterTest<T> {
 
-	
 	private Key[] keys;
 	private Pos[] pos;
-
 	
- 	
+	
 	public CounterTest() {
-		keys = new Key[100000];
-		pos = new Pos[100000];
+		keys = null;
+		pos = null;
 	}
+	
 	
 	public static void main(String[] args) {
-		//testSetA();
-		testSetB();
+		CounterTest ct = new CounterTest();
+		
+		//Test Set A  DeleteKey
+		ct.testSetA(10,true);
+		ct.testSetA(100,true);
+		ct.testSetA(1000,true);
+		ct.testSetA(10000,true);
+		ct.testSetA(100000,true);
+		
+		//Test Set A  DeletePos
+		ct.testSetA(10,false);
+		ct.testSetA(100,false);
+		ct.testSetA(1000,false);
+		ct.testSetA(10000,false);
+		ct.testSetA(100000,false);
+		
+		//Test Set B DeleteKey
+		ct.testSetB(10,true);
+		ct.testSetB(100,true);
+		ct.testSetB(1000,true);
+		ct.testSetB(10000,true);
+		ct.testSetB(100000,true);
+		
+		//Test Set B DeletePos
+		ct.testSetB(10,true);
+		ct.testSetB(100,true);
+		ct.testSetB(1000,true);
+		ct.testSetB(10000,true);
+		ct.testSetB(100000,true);
+		
+		//Test Set C  DeleteKey
+		ct.testSetC(10,true);
+		ct.testSetC(100,true);
+		ct.testSetC(1000,true);
+		ct.testSetC(10000,true);
+		ct.testSetC(100000,true);
+		
+		//Test Set C  DeletePos
+		ct.testSetC(10,true);
+		ct.testSetC(100,true);
+		ct.testSetC(1000,true);
+		ct.testSetC(10000,true);
+		ct.testSetC(100000,true);
+		
 		
 	}
 	
-	public static void testSetA() {
-		CounterTest ct = new CounterTest();
-		SetA<Integer> setA = new SetA<Integer>();
-		SetB1<Integer> setB1 = new SetB1<Integer>();
-		SetC<Integer> setC = new SetC<Integer>();
- 		
-		ct.add(10, setA);
-		ct.deleteKey(10, setA);
-		System.out.println("deleteKey 10      " + setA.getCounter().getCount());
+	public void testSetA(int size, boolean key) {
+		keys = new Key[100000];
+		pos = new Pos[100000];
+		SetA<Integer> setABeginning = new SetA<Integer>();
+		SetA<Integer> setAMiddle = new SetA<Integer>();
+		SetA<Integer> setAEnd = new SetA<Integer>();
+		add(size, setABeginning);
+		System.out.println("success");
+		add(size,setAMiddle);
+		System.out.println("success");
+		add(size,setAEnd);
+		System.out.println("success");
+		setABeginning.getCounter().reset();
+		setAMiddle.getCounter().reset();
+		setAEnd.getCounter().reset();
 		
-		setA = new SetA<Integer>();
-		ct.add(10, setA);
-		ct.deletePos(10, setA);
-		System.out.println("deletePos 10      " + setA.getCounter().getCount());
-		
-		setA = new SetA<Integer>();
-		ct.add(100, setA);
-		ct.deleteKey(100, setA);
-		System.out.println("deleteKey 100      " + setA.getCounter().getCount());
-		
-		setA = new SetA<Integer>();
-		ct.add(100, setA);
-		ct.deletePos(100, setA);
-		System.out.println("deletePos 100      " + setA.getCounter().getCount());
-		
-		setA = new SetA<Integer>();
-		ct.add(1000, setA);
-		ct.deleteKey(1000, setA);
-		System.out.println("deleteKey 1000      " + setA.getCounter().getCount());
-		
-		setA = new SetA<Integer>();
-		ct.add(1000, setA);
-		ct.deletePos(1000, setA);
-		System.out.println("deletePos 1000      " + setA.getCounter().getCount());
-		
-		setA = new SetA<Integer>();
-		ct.add(10000, setA);
-		ct.deleteKey(10000, setA);
-		System.out.println("deleteKey 10000      " + setA.getCounter().getCount());
-		
-		setA = new SetA<Integer>();
-		ct.add(10000, setA);
-		ct.deletePos(10000, setA);
-		System.out.println("deletePos 10000      " + setA.getCounter().getCount());
-		
-		setA = new SetA<Integer>();
-		ct.add(100000, setA);
-		ct.deleteKey(100000, setA);
-		System.out.println("deleteKey 100000      " + setA.getCounter().getCount());
-		
-		setA = new SetA<Integer>();
-		ct.add(100000, setA);
-		ct.deletePos(100000, setA);
-		System.out.println("deletePos 100000      " + setA.getCounter().getCount());
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		setC = new SetC<Integer>();
-		ct.add(10, setC);
-		ct.deletePos(10, setC);
-		System.out.println("deletePos 10      " + setC.getCounter().getCount());
-		
-		setC = new SetC<Integer>();
-		ct.add(100, setC);
-		ct.deleteKey(100, setC);
-		System.out.println("deleteKey 100      " + setC.getCounter().getCount());
-		
-		setC = new SetC<Integer>();
-		ct.add(100, setC);
-		ct.deletePos(100, setC);
-		System.out.println("deletePos 100      " + setC.getCounter().getCount());
-		
-		setC = new SetC<Integer>();
-		ct.add(1000, setC);
-		ct.deleteKey(1000, setC);
-		System.out.println("deleteKey 1000      " + setC.getCounter().getCount());
-		
-		setC = new SetC<Integer>();
-		ct.add(1000, setC);
-		ct.deletePos(1000, setC);
-		System.out.println("deletePos 1000      " + setC.getCounter().getCount());
-		
-		setC = new SetC<Integer>();
-		ct.add(10000, setC);
-		ct.deleteKey(10000, setC);
-		System.out.println("deleteKey 10000      " + setC.getCounter().getCount());
-		
-		setC = new SetC<Integer>();
-		ct.add(10000, setC);
-		ct.deletePos(10000, setC);
-		System.out.println("deletePos 10000      " + setC.getCounter().getCount());
-		
-		setC = new SetC<Integer>();
-		ct.add(100000, setC);
-		ct.deleteKey(100000, setC);
-		System.out.println("deleteKey 100000      " + setC.getCounter().getCount());
-		
-		setC = new SetC<Integer>();
-		ct.add(100000, setC);
-		ct.deletePos(1000, setC);
-		System.out.println("deletePos 100000      " + setC.getCounter().getCount());
-		
+		if (key) {
+			setABeginning.delete(keys[0]);
+			System.out.println("deleteKeyBeginning - " + " - " + size  + " - " + setABeginning.getCounter().getCount());
+			setAMiddle.delete(keys[(size-1)/2]);
+			System.out.println("deleteKeyEiddle - " + " - " + size + " - " + setAMiddle.getCounter().getCount());
+			setAEnd.delete(keys[size-1]);
+			System.out.println("deleteKeyEnd - " + " - " + size + " - " + setAEnd.getCounter().getCount());
+		}
+		else {
+			setABeginning.delete(pos[0]);
+			System.out.println("deletePosBeginning - " + size + setABeginning.getCounter().getCount());
+			setAMiddle.delete(pos[(size-1)/2]);
+			System.out.println("deletePosMiddle - " + size + setAMiddle.getCounter().getCount());
+			setAEnd.delete(pos[size-1]);
+			System.out.println("deletePosEnd - " + size + setAEnd.getCounter().getCount());
+		}
 	}
 	
-	public static void testSetB() {
-		CounterTest ct = new CounterTest();
-		SetB1<Integer> setB1 = new SetB1<Integer>();
+	public void testSetB(int size, boolean key) {
+		keys = new Key[100000];
+		pos = new Pos[100000];
+		SetB1<Integer> setBBeginning = new SetB1<Integer>();
+		SetB1<Integer> setBMiddle = new SetB1<Integer>();
+		SetB1<Integer> setBEnd = new SetB1<Integer>();
+		add(size, setBBeginning);
+		System.out.println("success");
+		add(size,setBMiddle);
+		System.out.println("success");
+		add(size,setBEnd);
+		System.out.println("success");
+		setBBeginning.getCounter().reset();
+		setBMiddle.getCounter().reset();
+		setBEnd.getCounter().reset();
 		
-		setB1 = new SetB1<Integer>();
-		ct.add(10, setB1);
-		ct.deletePos(10, setB1);
-		System.out.println("deletePos 10      " + setB1.getCounter().getCount());
-		
-		setB1 = new SetB1<Integer>();
-		ct.add(100, setB1);
-		ct.deleteKey(100, setB1);
-		System.out.println("deleteKey 100      " + setB1.getCounter().getCount());
-		
-		setB1 = new SetB1<Integer>();
-		ct.add(100, setB1);
-		ct.deletePos(100, setB1);
-		System.out.println("deletePos 100      " + setB1.getCounter().getCount());
-		
-		setB1 = new SetB1<Integer>();
-		ct.add(1000, setB1);
-		ct.deleteKey(1000, setB1);
-		System.out.println("deleteKey 1000      " + setB1.getCounter().getCount());
-		
-		setB1 = new SetB1<Integer>();
-		ct.add(1000, setB1);
-		ct.deletePos(1000, setB1);
-		System.out.println("deletePos 1000      " + setB1.getCounter().getCount());
-		
-		setB1 = new SetB1<Integer>();
-		ct.add(10000, setB1);
-		ct.deleteKey(10000, setB1);
-		System.out.println("deleteKey 10000      " + setB1.getCounter().getCount());
-		
-		setB1 = new SetB1<Integer>();
-		ct.add(10000, setB1);
-		ct.deletePos(10000, setB1);
-		System.out.println("deletePos 10000      " + setB1.getCounter().getCount());
-		
-		setB1 = new SetB1<Integer>();
-		ct.add(100000, setB1);
-		ct.deleteKey(100000, setB1);
-		System.out.println("deleteKey 100000      " + setB1.getCounter().getCount());
-		
-		setB1 = new SetB1<Integer>();
-		ct.add(100000, setB1);
-		ct.deletePos(1000, setB1);
-		System.out.println("deletePos 100000      " + setB1.getCounter().getCount());
+		if (key) {
+			setBBeginning.delete(keys[0]);
+			System.out.println("deleteKeyBeginning - " + " - " + size  + " - " + setBBeginning.getCounter().getCount());
+			setBMiddle.delete(keys[(size-1)/2]);
+			System.out.println("deleteKeyEiddle - " + " - " + size + " - " + setBMiddle.getCounter().getCount());
+			setBEnd.delete(keys[size-1]);
+			System.out.println("deleteKeyEnd - " + " - " + size + " - " + setBEnd.getCounter().getCount());
+		}
+		else {
+			setBBeginning.delete(pos[0]);
+			System.out.println("deletePosBeginning - " + size + setBBeginning.getCounter().getCount());
+			setBMiddle.delete(pos[(size-1)/2]);
+			System.out.println("deletePosMiddle - " + size + setBMiddle.getCounter().getCount());
+			setBEnd.delete(pos[size-1]);
+			System.out.println("deletePosEnd - " + size + setBEnd.getCounter().getCount());
+		}
 	}
+	
+	public void testSetC(int size, boolean key) {
+		keys = new Key[100000];
+		pos = new Pos[100000];
+		SetC<Integer> setCBeginning = new SetC<Integer>();
+		SetC<Integer> setCMiddle = new SetC<Integer>();
+		SetC<Integer> setCEnd = new SetC<Integer>();
+		add(size, setCBeginning);
+		System.out.println("success");
+		add(size,setCMiddle);
+		System.out.println("success");
+		add(size,setCEnd);
+		System.out.println("success");
+		setCBeginning.getCounter().reset();
+		setCMiddle.getCounter().reset();
+		setCEnd.getCounter().reset();
+		
+		if (key) {
+			setCBeginning.delete(keys[0]);
+			System.out.println("deleteKeyBeginning - " + " - " + size  + " - " + setCBeginning.getCounter().getCount());
+			setCMiddle.delete(keys[(size-1)/2]);
+			System.out.println("deleteKeyEiddle - " + " - " + size + " - " + setCMiddle.getCounter().getCount());
+			setCEnd.delete(keys[size-1]);
+			System.out.println("deleteKeyEnd - " + " - " + size + " - " + setCEnd.getCounter().getCount());
+		}
+		else {
+			setCBeginning.delete(pos[0]);
+			System.out.println("deletePosBeginning - " + size + setCBeginning.getCounter().getCount());
+			setCMiddle.delete(pos[(size-1)/2]);
+			System.out.println("deletePosMiddle - " + size + setCMiddle.getCounter().getCount());
+			setCEnd.delete(pos[size-1]);
+			System.out.println("deletePosEnd - " + size + setCEnd.getCounter().getCount());
+		}
+	}
+	
+	
 	
 
 
-	public void add(int amount, Set<T> set) {
-		Element elem = null;
+	public void add(int amount, Set<Integer> set) {
+		Element<Integer> elem = null;
 		
 		for(int i = 0; i<amount;i++) {
 			elem = new Element<Integer>(new Integer(i));
-			pos[i] = set.add((Element<T>)elem);
+			pos[i] = set.add((Element<Integer>)elem);
 			keys[i] = elem.getKey();
 		}
 	}
